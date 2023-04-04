21de523e6f1d8660f559cc70b02cdc48ab1ed73d
Nuutti Heinäsmäki <nuutti.heinasmaki@hotmail.fi>
2023-04-04 14:18:19 +0300

    Add tooltips for resource panels and update descriptions

---
+++ Assets/Scripts/UI/PopUpSmallTooltip.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+using TMPro;
+
+public class PopUpSmallTooltip : PopUpMenu, IPointerEnterHandler, IPointerExitHandler
+{
+    private OpenTooltipOnHover _hoverScript;
+    
+    public override void Init(GameObject bossObject, ObjectInfo objectInfo)
+    {
+        _bossObject = bossObject;
+        _hoverScript = _bossObject.GetComponent<OpenTooltipOnHover>();
+        _objectInfo = objectInfo;
+
+        // Move based on mouse position?
+
+        _image.sprite = _objectInfo.sprite;
+        _extraText.text = $"{_objectInfo.name}";
+        _textArea.text = $"{_objectInfo.description}";
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        Debug.Log("PopUpGenericTooltip.OnPointerEnter()");
+        _hoverScript.SetTooltipHoverState(true);
+    }
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        Debug.Log("PopUpGenericTooltip.OnPointerExit()");
+        _hoverScript.SetTooltipHoverState(false);
+    }
+}
